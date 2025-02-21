b5ba7ae478dcbdf794791534cb714f54dff0a356
afreenme-22 <arjumandafreen407@gmail.com>
2025-02-21 14:34:46 +0500

    Fixed projectId issue and updated client.ts

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -1,16 +1,12 @@
-import { createClient } from 'next-sanity'
-
-import { apiVersion, dataset, projectId } from '../env'
+import { createClient } from "next-sanity";
 
 export const client = createClient({
-  projectId:process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  dataset:process.env.NEXT_PUBLIC_SANITY_DATASET,
-  apiVersion:'2025-01-17',
-  useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
-  token:process.env.NEXT_PUBLIC_SANITY_TOKEN,
-  
-})
-
+  projectId: (process.env.NEXT_PUBLIC_SANITY_PROJECT_ID as string)?.toLowerCase(), // Ensures lowercase
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production", // Add default value if needed
+  apiVersion: "2025-01-17",
+  useCdn: true,
+  token: process.env.NEXT_PUBLIC_SANITY_TOKEN,
+});
 
 
 
